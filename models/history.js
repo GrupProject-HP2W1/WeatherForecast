d2d6883fbad1ff6e92201c51d86d19b2497f8255
agunggst <rqz.agastya@gmail.com>
2020-03-05 20:32:19 +0700

    1. Membuat model, serta mengatur asosiasinya 2. Password user menggunakan bcrypt 3. Membuat folder controller

---
+++ models/history.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  class History extends sequelize.Sequelize.Model {}
+
+  History.init({
+    city: DataTypes.STRING,
+    date: DataTypes.DATE,
+    lat: DataTypes.STRING,
+    long: DataTypes.STRING,
+    user_id: DataTypes.INTEGER
+  }, {sequelize})
+
+  History.associate = function(models) {
+    History.belongsTo( models.User, {foreignKey:'user_id'} )
+  };
+  return History;
+};
